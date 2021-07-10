e4ac01e89ca5f6a4bca4f896e56810abe4bb1674
Malith Priyashan <malithcode@gmail.com>
2021-07-10 23:55:44 +0200

    multi zone

---
+++ marketplace/next.config.js
@@ -0,0 +1,21 @@
+const { BLOG_URL } = process.env
+
+module.exports = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/blog',
+        destination: `${BLOG_URL}/blog`,
+      },
+      {
+        source: '/blog/:path*',
+        destination: `${BLOG_URL}/blog/:path*`,
+      },
+    ]
+  },
+}
